c542e572984a6fc9b07516828d9e95c204a8e7d8
Juminji-117 <alswl0w0@gmail.com>
2022-06-28 15:57:30 +0900

    사이트 제목 꾸미기

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,15 @@
 import React from "react";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 
 function AboutPage() {
   return (
-    <main>
-      <Layout>
-      <title>자기소개</title>
-      <h1>자기소개 페이지 입니다.</h1>
-      </Layout>
-    </main>
-  );
+
+  <Layout pageTitle="About Me">
+  <p>
+    Hi there! I'm the proud creator of this site, which I built with Gatsby.
+  </p>
+  </Layout>
+        );
 }
 
 export default AboutPage;
