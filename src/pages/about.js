3329aa8348bbc82d6dce6c48754a8715e9df535e
Juminji-117 <alswl0w0@gmail.com>
2022-06-28 13:08:45 +0900

    GNB 추가

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,16 @@
+import { Link } from "gatsby";
 import React from "react";
 
 function AboutPage() {
   return (
     <main>
       <title>자기소개</title>
+      <div style={{display:"flex", gap:20}}>
+      <Link href="/">홈</Link>
+      <Link href="/about">어바웃</Link>
+      <Link href="/test">테스트</Link>
+      <Link href="test-space">테스트-스페이스</Link>
+      </div>
     <h1>자기소개 페이지 입니다.</h1>
     </main>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
+import { Link } from "gatsby";
 import React from "react"
 
 
 const IndexPage = () => {
   return (
     <main>
-      <title>Home Page</title> 
+      <title>Home Page</title>
+      <div style={{display:"flex", gap:20}}>
+      <Link href="/">홈</Link>
+      <Link href="/about">어바웃</Link>
+      <Link href="/test">테스트</Link>
+      <Link href="test-space">테스트-스페이스</Link>
+      </div>
       <h1>Home Page</h1>
     </main>
   )
